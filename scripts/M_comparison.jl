0fd9e5778843fb258ac0c84129a0bbc321948494
DrWatson <no@mail>
2022-07-20 10:42:28 +0200

    M MaxEpochs adapted

--- scripts/M_comparison.jl
+++ scripts/M_comparison.jl
@@ -12,29 +12,32 @@ include(srcdir("dnn_decoder.jl"))
 ##
 
 ##
-function dnn_dec(σi; MaxEpochs=2000,bs=128,opt=ADAM)
+function M_comp(σi; MaxEpochs=2000,bs=128,opt=ADAM,MaxPasses = 2*10E7)
     t = ScaleTransform(1/(sqrt(2)*σi))
     #Kernel matrices
     K = kernelmatrix(k,t(vcat(x_samples,x_m)))
     #Parameters to datapoint ratio
-    ddecDicts = Dict(Md=> Dict() for M=1:MVec)
+    ddecDicts = Dict()
     idx_tst = rand(1:length(x_samples),n_tst)
+    #Generate data
+    V = mvn_sample(K,N);
+    V_m = V[:,end-M+1:end] 
     x_tst = x_samples[idx_tst]
     R_tst = V[:,idx_tst] + sqrt(η)*randn(N,n_tst)
     data_tst = Flux.Data.DataLoader((Float32.(R_tst),x_tst'),
             batchsize = 512,shuffle = false);
-    #Generate data
-    V = mvn_sample(K,N);
-    V_m = V[:,end-M+1:end] 
     ddecDicts[:tc] = V_m
     #Ideal error
     ε_id = mse_ideal(V_m,η,x_m,R_tst,x_tst')
     #Store: pecoder, ideal error, history of training and tuning curves
     ddecDicts[:ε_id] =ε_id
+    ddecDicts[:dnn] = Dict(Md=> Dict() for Md=1:MVec)
     for Md=MVec
-        ddecD = ddecDicts[Md]
+        ddecD = ddecDicts[:dnn][Md]
         P = Md*N*γ
-        @info Md,P,σi
+        #Adapt MaxEpochs to number of training points
+        MaxEpochs = Int(round(MaxPasses/P))
+        @info Md,P,σi,MaxEpochs
         idx_trn = rand(1:length(x_samples),P)
         x_trn = x_samples[idx_trn]
         R_trn = V[:,idx_trn] + sqrt(η)*randn(N,P)
@@ -47,7 +50,7 @@ function dnn_dec(σi; MaxEpochs=2000,bs=128,opt=ADAM)
             Dense(Md,1,identity))
         #Train decoder and computes ideal error
         dec, history = train_dnn_dec(data,dec=mydec,
-            epochs=MaxEpochs,min_diff=1f-7)
+            epochs=MaxEpochs,min_diff=1f-8)
         ddecD[:dnn_dec] = dec
         ddecD[:history] = history
         @info "Finished" Md  
@@ -55,7 +58,7 @@ function dnn_dec(σi; MaxEpochs=2000,bs=128,opt=ADAM)
     return ddecDicts
 end
 ##
-nets =8   #Numbers of test networks
+nets =4   #Numbers of test networks
 #Dataset parameters
 P_i= 2000  #How many I can sample from (not so important)
 n_tst = Int.(1e5)
@@ -72,15 +75,14 @@ x_m = bin[1:end-1] .+ diff(bin)/2
 # Network parameters
 σVec = (5:8:54)/500
 N =  50
-MVec = Int.(round.(10 .^(range(1,3,length=7))))
-
+MVec = [10,25,50,100,250,500,1000]#Int.(round.(10 .^(range(1,3,length=7))))
 k =SqExponentialKernel()
 η = 0.3
 ## Run simulations
-d_dec  = Dict((σi,n) => dnn_dec(σi) for σi = σVec, n=1:nets)
+d_dec  = Dict((σi,n) => M_comp(σi) for σi = σVec, n=1:nets)
 #Save results
 ##
-Nmin,Nmax = first(NVec),last(NVec)
-name = savename("dnn_dec" , (@dict Nmin Nmax γ η Md α),"jld2")
+Mmin,Mmax = first(MVec),last(MVec)
+name = savename("Mcomp" , (@dict N γ η Mmax Mmin α),"jld2")
 data = Dict("NVec"=>NVec ,"σVec" => σVec,"dnn_decoder" => d_dec)
 safesave(datadir("sims/dnn_decoder",name) ,data)
